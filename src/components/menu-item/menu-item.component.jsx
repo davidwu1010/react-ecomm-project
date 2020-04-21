21b10c8836ea79108dc8ff9038b15a5b8435725d
David Wu <wudawei01@gmail.com>
2020-04-21 22:36:06 +0100

    Fix a bug

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
-    <div className={size ? "large menu-item" : "menu-item"} onClick={() => history.push(`/shop/mens`)}>
+    <div className={size ? "large menu-item" : "menu-item"} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}></div>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
