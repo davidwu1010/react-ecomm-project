599020296ccf636b0f934b692f03397709ad2db8
David Wu <wudawei01@gmail.com>
2020-05-12 16:02:52 +0100

    Fix

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyAKP2Vzn5lz2w0xnSHk_VCNz_JkR6d5lGg",
+  authDomain: "crwn-db-9830a.firebaseapp.com",
+  databaseURL: "https://crwn-db-9830a.firebaseio.com",
+  projectId: "crwn-db-9830a",
+  storageBucket: "crwn-db-9830a.appspot.com",
+  messagingSenderId: "524522125079",
+  appId: "1:524522125079:web:d1b32a889312364adfe952",
+  measurementId: "G-HPDMLDECSZ"
 };
 
 firebase.initializeApp(config);
